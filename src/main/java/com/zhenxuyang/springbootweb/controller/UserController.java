17baeea7431211dc72134a810bd797f7bd647f8c
zhenxuyang@sjtu.edu.cn <zhenxuyang@sjtu.edu.cn>
2023-02-27 21:55:09 +0800

    update

--- src/main/java/com/zhenxuyang/springbootweb/controller/UserController.java
+++ src/main/java/com/zhenxuyang/springbootweb/controller/UserController.java
@@ -1,5 +1,9 @@
 package com.zhenxuyang.springbootweb.controller;
 
+import com.zhenxuyang.springbootweb.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.stereotype.Controller;
 
@@ -14,5 +18,11 @@ import org.springframework.stereotype.Controller;
 @Controller
 @RequestMapping("/user")
 public class UserController {
+    @Autowired
+    IUserService userService;
+    @GetMapping("/{id}")
+    public Object getById(@PathVariable("id") Long id){
+        return userService.getById(id);
+    }
 
 }
